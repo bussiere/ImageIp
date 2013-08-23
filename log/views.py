29883dc0ce8082a566d671d39c3b5e9f3dfaba82
bussiere <bussiere@gmail.com>
2013-08-23 11:01:31 +0200

    again

--- log/views.py
+++ log/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 def logo(request):
+    ip = request.META.get('REMOTE_ADDR')
+    print ip
+    referer = request.META.get('HTTP_REFERER')
+    print referer
+    host = request.META.get('HTTP_HOST')
+    print host
+    useragent = request.META.get('HTTP_USER_AGENT')
+    print useragent
     OPENID_LOGO_BASE_64 = """
     R0lGODlhEAAQAMQAAO3t7eHh4srKyvz8/P5pDP9rENLS0v/28P/17tXV1dHEvPDw8M3Nzfn5+d3d
     3f5jA97Syvnv6MfLzcfHx/1mCPx4Kc/S1Pf189C+tP+xgv/k1N3OxfHy9NLV1/39/f///yH5BAAA
